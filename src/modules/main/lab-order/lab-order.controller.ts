062297928b8a875ca634a8626fcc82b02c862377
Greiber Salas <greibersalas@gmail.com>
2021-03-26 19:57:00 -0500

    Base pdf reports

--- src/modules/main/lab-order/lab-order.controller.ts
+++ src/modules/main/lab-order/lab-order.controller.ts
@@ -3,6 +3,9 @@ import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from '@
 import { LabOrder } from './lab-order.entity';
 import { LabOrderService } from './lab-order.service';
 
+//FPDF
+const FPDF = require('./pdf-barcode');
+//import * as FPDF from './pdf-barcode';
 @Controller('lab-order')
 export class LabOrderController {
 
@@ -60,5 +63,64 @@ export class LabOrderController {
     async confirm(@Param('id',ParseIntPipe) id: number,@Param('state',ParseIntPipe) state: number): Promise<any>{
         const confirm = await this._labOrderService.confirm(id,state);
         return confirm;
+    }
+
+    @Get('pdf/:id')
+    async getPdf(@Param('id',ParseIntPipe) id: number): Promise<any>{
+        let dm: number[] = [50,40];
+        const pdf = new FPDF('L','mm',dm);
+        pdf.AddPage('L','A4');
+        pdf.SetTitle('Test Lab');
+        //pdf.SetLeftMargin(15);
+        //pdf.SetRightMargin(15);
+        pdf.SetFillColor(200,200,200);
+
+        pdf.SetFont('Arial','',40);
+        pdf.SetY(10);
+        pdf.SetX(10);
+        pdf.Cell(260,40,'',1,0,'L');
+        pdf.SetY(10);
+        pdf.SetX(10);
+        pdf.Cell(260,20,'Nombre:',0,0,'L');
+        pdf.SetFont('Arial','B',40);
+        pdf.SetY(30);
+        pdf.SetX(10);
+        pdf.Cell(260,20,'Greiber Jesús Salas Coronel',0,0,'L');
+
+        pdf.SetFont('Arial','',40);
+        pdf.SetY(50);
+        pdf.SetX(10);
+        pdf.Cell(260,40,'',1,0,'L');
+        pdf.SetY(50);
+        pdf.SetX(10);
+        pdf.Cell(260,20,'Historia:',0,0,'L');
+        pdf.SetFont('Arial','B',40);
+        pdf.SetY(70);
+        pdf.SetX(10);
+        pdf.Cell(260,20,'MX121-123456',0,0,'L');
+
+        pdf.SetFont('Arial','',40);
+        pdf.SetY(90);
+        pdf.SetX(10);
+        pdf.Cell(130,40,'',1,0,'L');
+        pdf.SetY(90);
+        pdf.SetX(10);
+        pdf.Cell(130,20,'Fecha: ',0,0,'L');
+        pdf.SetFont('Arial','B',40);
+        pdf.SetY(110);
+        pdf.SetX(10);
+        pdf.Cell(130,20,'26-03-2021',0,0,'L');
+
+        /* pdf.SetY(90);
+        pdf.SetX(140);
+        pdf.Cell(130,40,'',1,0,'L'); */
+
+        pdf.Image('assets/img/logo.jpg',140,90,130,40,'jpg');
+
+        pdf.Code39(10,150,'MX121-123456',3,30);
+
+        pdf.Output('F','uploads/pdf/lab/test.pdf');
+
+        return 'uploads/pdf/lab/test.pdf'
     }
 }
