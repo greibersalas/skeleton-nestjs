617426f4e8bb44747b2bcd63e40a445cc19efd6f
Greiber Salas <greibersalas@gmail.com>
2021-05-09 12:41:10 -0500

    Audit medical act

--- src/modules/main/medical-act/medical-act.controller.ts
+++ src/modules/main/medical-act/medical-act.controller.ts
@@ -1,14 +1,18 @@
-import { Body, Controller, Delete, Get, HttpStatus, Param, ParseIntPipe, Post, Put, Res, UploadedFile, UseInterceptors } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, Param, ParseIntPipe, Post, Put, Res, UploadedFile, UseGuards, UseInterceptors, Request } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { extname } from 'path';
 import { diskStorage } from 'multer';
+import { JwtAuthGuard } from '../../auth/strategies/jwt-auth.guard';
+var moment = require('moment-timezone');
 
 import { editFileName, imageFileFilter } from '../../../utils/file-upload.utils';
+import { Audit } from '../../security/audit/audit.entity';
 import { MedicalAct } from './medical-act.entity';
 import { MedicalActService } from './medical-act.service';
 import { MedicalActFileGroup } from './medical-act-file-group.entity';
 import { MedicalActFiles } from './medical-act-files.entity';
 
+@UseGuards(JwtAuthGuard)
 @Controller('medical-act')
 export class MedicalActController {
 
@@ -27,25 +31,71 @@ export class MedicalActController {
     }
 
     @Post()
-    async createMedicalAct(@Body() medicalAct: MedicalAct): Promise<MedicalAct>{
+    async createMedicalAct(
+        @Body() medicalAct: MedicalAct,
+        @Request() req: any
+    ): Promise<MedicalAct>{
         const create = await this._medicalActService.create(medicalAct);
+        //Creamos los datos de la auditoria
+        const audit = new Audit();
+        audit.idregister = create.id;
+        audit.title = 'medical-act';
+        audit.description = 'Insertar registro';
+        audit.data = JSON.stringify(create);
+        audit.iduser = Number(req.user.id);
+        audit.datetime = moment().format('YYYY-MM-DD HH:mm:ss');
+        audit.state = 1;
+        //Guardamos la auditoria
+        await audit.save();
+        //Respondemos al usuario
         return create;
     }
 
     @Put(':id')
-    async updateMedicalAct(@Param('id',ParseIntPipe) id: number, @Body() medicalAct: MedicalAct){
+    async updateMedicalAct(
+        @Param('id',ParseIntPipe) id: number,
+        @Body() medicalAct: MedicalAct,
+        @Request() req: any
+    ){
         const update = await this._medicalActService.update(id,medicalAct);
+        //Creamos los datos de la auditoria
+        const audit = new Audit();
+        audit.idregister = id;
+        audit.title = 'medical-act';
+        audit.description = 'Update registro';
+        audit.data = JSON.stringify(update);
+        audit.iduser = Number(req.user.id);
+        audit.datetime = moment().format('YYYY-MM-DD HH:mm:ss');
+        audit.state = 1;
+        //Guardamos la auditoria
+        await audit.save();
+        //Respondemos al usuario
         return update;
     }
 
     @Delete(':id')
-    async deleteMedicalAct(@Param('id',ParseIntPipe) id: number){
+    async deleteMedicalAct(
+        @Param('id',ParseIntPipe) id: number,
+        @Request() req: any
+    ){
         await this._medicalActService.delete(id);
+        //Creamos los datos de la auditoria
+        const audit = new Audit();
+        audit.idregister = id;
+        audit.title = 'medical-act';
+        audit.description = 'Delete registro';
+        audit.data = null;
+        audit.iduser = Number(req.user.id);
+        audit.datetime = moment().format('YYYY-MM-DD HH:mm:ss');
+        audit.state = 1;
+        //Guardamos la auditoria
+        await audit.save();
+        //Respondemos al usuario
         return true;
     }
 
     @Get('get-by-reservation/:id')
-    async getFirst(@Param('id') id): Promise<MedicalAct>{
+    async getFirst(@Param('id', ParseIntPipe) id: number): Promise<MedicalAct>{
         return await this._medicalActService.getByReservation(id);
     }
 
@@ -60,9 +110,13 @@ export class MedicalActController {
             fileFilter: imageFileFilter,
         }),
     )
-    async uploadFile(@UploadedFile() file,@Param('group',ParseIntPipe) group: number,
-    @Param('id',ParseIntPipe) id: number,@Body() des: any){
-        
+    async uploadFile(
+        @UploadedFile() file: any,
+        @Param('group',ParseIntPipe) group: number,
+        @Param('id',ParseIntPipe) id: number,
+        @Body() des: any,
+        @Request() req: any
+    ){
         const response = {
             originalname: file.originalname,
             filename: file.filename,
@@ -76,7 +130,19 @@ export class MedicalActController {
             filegroup: group,
             desciption: des.description
         };
-        await this._medicalActService.addFiles(data);
+        const fileinsert = await this._medicalActService.addFiles(data);
+        //Creamos los datos de la auditoria
+        const audit = new Audit();
+        audit.idregister = fileinsert.id;
+        audit.title = 'medical-act-file';
+        audit.description = 'Insertar registro';
+        audit.data = JSON.stringify(fileinsert);
+        audit.iduser = Number(req.user.id);
+        audit.datetime = moment().format('YYYY-MM-DD HH:mm:ss');
+        audit.state = 1;
+        //Guardamos la auditoria
+        await audit.save();
+        //Respondemos al usuario
         return {
             status: HttpStatus.OK,
             message: 'Image uploaded successfully!',
@@ -85,7 +151,7 @@ export class MedicalActController {
     }
 
     @Get('get-file/:file')
-    getFile(@Param('file') file, @Res() res){
+    getFile(@Param('file') file: any, @Res() res: any){
         const response = res.sendFile(file,{root: './uploads'});
         return {
             status: HttpStatus.OK,
@@ -101,14 +167,45 @@ export class MedicalActController {
     }
 
     @Post('group')
-    async createGroup(@Body() medicalActFileGroup: MedicalActFileGroup): Promise<MedicalActFileGroup>{
+    async createGroup(
+        @Body() medicalActFileGroup: MedicalActFileGroup,
+        @Request() req: any
+    ): Promise<MedicalActFileGroup>{
         const create = await this._medicalActService.createGroup(medicalActFileGroup);
+        //Creamos los datos de la auditoria
+        const audit = new Audit();
+        audit.idregister = create.id;
+        audit.title = 'group-file';
+        audit.description = 'Insertar registro';
+        audit.data = JSON.stringify(create);
+        audit.iduser = Number(req.user.id);
+        audit.datetime = moment().format('YYYY-MM-DD HH:mm:ss');
+        audit.state = 1;
+        //Guardamos la auditoria
+        await audit.save();
+        //Respondemos al usuario
         return create;
     }
 
     @Put('group/:id')
-    async updateGroup(@Param('id',ParseIntPipe) id: number, @Body() data: MedicalActFileGroup){
+    async updateGroup(
+        @Param('id',ParseIntPipe) id: number,
+        @Body() data: MedicalActFileGroup,
+        @Request() req: any
+    ){
         const update = await this._medicalActService.updateGroup(id,data);
+        //Creamos los datos de la auditoria
+        const audit = new Audit();
+        audit.idregister = id;
+        audit.title = 'group-file';
+        audit.description = 'Update registro';
+        audit.data = JSON.stringify(update);
+        audit.iduser = Number(req.user.id);
+        audit.datetime = moment().format('YYYY-MM-DD HH:mm:ss');
+        audit.state = 1;
+        //Guardamos la auditoria
+        await audit.save();
+        //Respondemos al usuario
         return update;
     }
 
