8cfb798147163b98f93608852591aa91f6dc2d39
Greiber Salas <greibersalas@gmail.com>
2021-05-09 11:31:23 -0500

    Audit anamnesis, attention card

--- src/modules/main/anamnesis/anamnesis.controller.ts
+++ src/modules/main/anamnesis/anamnesis.controller.ts
@@ -1,8 +1,12 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Put, UseGuards, Request } from '@nestjs/common';
+var moment = require('moment-timezone');
+import { JwtAuthGuard } from '../../auth/strategies/jwt-auth.guard';
 
+import { Audit } from '../../security/audit/audit.entity';
 import { Anamnesis } from './anamnesis.entity';
 import { AnamnesisService } from './anamnesis.service';
 
+@UseGuards(JwtAuthGuard)
 @Controller('anamnesis')
 export class AnamnesisController {
 
@@ -21,20 +25,66 @@ export class AnamnesisController {
     }
 
     @Post()
-    async createAnamnesis(@Body() anamnesis: Anamnesis): Promise<Anamnesis>{
+    async createAnamnesis(
+        @Body() anamnesis: Anamnesis,
+        @Request() req: any
+    ): Promise<Anamnesis>{
         const create = await this._anamnesisService.create(anamnesis);
+        //Creamos los datos de la auditoria
+        const audit = new Audit();
+        audit.idregister = create.id;
+        audit.title = 'Anamnesis';
+        audit.description = 'Insert registro';
+        audit.data = JSON.stringify(create);
+        audit.iduser = Number(req.user.id);
+        audit.datetime = moment().format('YYYY-MM-DD HH:mm:ss');
+        audit.state = 1;
+        //Guardamos la auditoria
+        await audit.save();
+        //Respondemos al usuario
         return create;
     }
 
     @Put(':id')
-    async updateAnamnesis(@Param('id',ParseIntPipe) id: number, @Body() anamnesis: Anamnesis){
+    async updateAnamnesis(
+        @Param('id',ParseIntPipe) id: number,
+        @Body() anamnesis: Anamnesis,
+        @Request() req: any
+    ){
         const update = await this._anamnesisService.update(id,anamnesis);
+        //Creamos los datos de la auditoria
+        const audit = new Audit();
+        audit.idregister = id;
+        audit.title = 'anamnesis';
+        audit.description = 'Update registro';
+        audit.data = JSON.stringify(update);
+        audit.iduser = Number(req.user.id);
+        audit.datetime = moment().format('YYYY-MM-DD HH:mm:ss');
+        audit.state = 1;
+        //Guardamos la auditoria
+        await audit.save();
+        //Respondemos al usuario
         return update;
     }
 
     @Delete(':id')
-    async deleteAnamnesis(@Param('id',ParseIntPipe) id: number){
+    async deleteAnamnesis(
+        @Param('id',ParseIntPipe) id: number,
+        @Request() req: any
+    ){
         await this._anamnesisService.delete(id);
+        //Creamos los datos de la auditoria
+        const audit = new Audit();
+        audit.idregister = id;
+        audit.title = 'anamnesis';
+        audit.description = 'Delete registro';
+        audit.data = null;
+        audit.iduser = Number(req.user.id);
+        audit.datetime = moment().format('YYYY-MM-DD HH:mm:ss');
+        audit.state = 1;
+        //Guardamos la auditoria
+        await audit.save();
+        //Respondemos al usuario
         return true;
     }
 
