7cd7dbc57b06b6144c2188b325e390d5396e6e2e
Greiber Salas <greibersalas@gmail.com>
2021-05-25 08:34:26 -0500

    [Fix] upload file

--- src/utils/file-upload.utils.ts
+++ src/utils/file-upload.utils.ts
@@ -2,7 +2,7 @@ import { extname } from 'path';
 import { HttpException, HttpStatus } from '@nestjs/common';
 
 // Allow only images
-export const imageFileFilter = (req, file, callback) => {
+export const imageFileFilter = (req: any, file: any, callback: any) => {
   if (!file.originalname.match(/\.(jpg|jpeg|png|gif|doc|docx|xls|xlsx|pdf|mp3|mp4|zip)$/)) {
     return callback(
       new HttpException(
@@ -15,14 +15,14 @@ export const imageFileFilter = (req, file, callback) => {
   callback(null, true);
 };
 
-export const editFileName = (req: any, file, callback) => {
+export const editFileName = (req: any, file: any, callback: any) => {
   //console.log("request ", req.params);
-  const { group } = req.params;
+  const { id, group } = req.params;
   const name = file.originalname.split('.')[0];
   const fileExtName = extname(file.originalname);
   const randomName = Array(10)
     .fill(null)
     .map(() => Math.round(Math.random() * 10).toString(10))
     .join('');
-  callback(null, `${group}-${randomName}${fileExtName}`);
+  callback(null, `${id}-${group}-${randomName}${fileExtName}`);
 };
