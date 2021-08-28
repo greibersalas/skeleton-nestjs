4eaee5c24160e506b80a2746c63eba0f09404530
Greiber Salas <greibersalas@gmail.com>
2021-08-27 21:03:58 -0500

    Report top 10 tariff

--- src/modules/main/medical-act-attention/medical-act-attention.service.ts
+++ src/modules/main/medical-act-attention/medical-act-attention.service.ts
@@ -1,5 +1,6 @@
 import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { getManager } from 'typeorm';
 import { MedicalActAttention } from './medical-act-attention.entity';
 import { MedicalActAttentionRepository } from './medical-act-attention.repository';
 
@@ -77,5 +78,74 @@ export class MedicalActAttentionService {
         .where('EXTRACT(month FROM "date") = :month AND EXTRACT(YEAR FROM "date") = :year AND state <> 0',{month,year})
         .getCount();
         return cant;
+    }
+
+    //Reports
+    /**
+     * Metodo para retornar la lista de
+     * tratamientos realizados en un día
+     * @param date
+     * @param specialty
+     * @param businessline
+     * @return
+     */
+    async treatmentRealized(
+        date: string,
+        specialty: number,
+        businessline: number,
+        doctor: number
+    ): Promise<any>{
+        let where = `maa.date = :date AND maa.state = 1 `;
+        if(businessline > 0){
+            where += `AND "maa"."businesslineId" = ${businessline} `;
+        }
+        if(specialty > 0){
+            where += ` AND "maa"."specialtyId" = ${specialty}`;
+        }
+        if(doctor > 0){
+            where += ` AND "maa"."doctorId" = ${doctor}`;
+        }
+        const data = await this._medicalActAttentionRepository.createQueryBuilder('maa')
+        .select(`"maa"."tariffId",
+        tr.description AS tratamiento,
+        "maa"."doctorId" as iddoctor,
+        count("maa"."tariffId")::NUMERIC as cantidad,
+        sum(maa.value) as total`)
+        .innerJoin('maa.tariff','tr')
+        .where(`${where}`,{date})
+        .groupBy('"maa"."tariffId",tr.description,"maa"."doctorId"')
+        .orderBy('tr.description')
+        .getRawMany();
+        return data;
+    }
+
+    async top10Tariff(
+        since: string,
+        until: string
+    ): Promise<any>{
+        const q1 = this._medicalActAttentionRepository.createQueryBuilder('maa')
+        .select(`"maa"."businesslineId" AS bl,
+        "maa"."tariffId",
+        tr.description AS tratamiento,
+        count("maa"."tariffId")::NUMERIC as cantidad,
+        sum(maa.value) as total`)
+        .where(`maa.date BETWEEN '${since}' AND '${until}' AND maa.state = 1
+        and "maa"."businesslineId" = 2`)
+        .innerJoin('maa.tariff','tr')
+        .groupBy('"maa"."tariffId","maa"."businesslineId",tr.description')
+        .getQuery();
+        const q2 = this._medicalActAttentionRepository.createQueryBuilder('maa')
+        .select(`"maa"."businesslineId" AS bl,
+        "maa"."tariffId",
+        tr.description AS tratamiento,
+        count("maa"."tariffId")::NUMERIC as cantidad,
+        sum(maa.value) as total`)
+        .where(`maa.date BETWEEN '${since}' AND '${until}' AND maa.state = 1
+        and "maa"."businesslineId" = 3`)
+        .innerJoin('maa.tariff','tr')
+        .groupBy('"maa"."tariffId","maa"."businesslineId",tr.description')
+        .getQuery();
+        const entityManager = getManager();
+        return await entityManager.query(`SELECT * FROM (${ q1 } UNION ALL ${ q2 }) AS foo ORDER BY foo.bl,foo.cantidad ASC, foo.tratamiento`)
     }
 }
