dc6f32d9a2cf71629ce593388822164962ad15bb
Greiber Salas <greibersalas@gmail.com>
2022-03-02 14:33:46 -0500

    port DB

--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -15,7 +15,7 @@ export const databaseProviders = [
                 host: config.get(Configuration.HOST),
                 username: config.get(Configuration.USER),                
                 password: config.get(Configuration.PASSWORD),
-                port : 5444,
+                port : 5432,
                 database: config.get(Configuration.DATABASE),
                 entities: [__dirname + '/../**/*.entity{.ts,.js}'],
                 migrations: [__dirname + '/migrations/*.{.ts,.js}'],
