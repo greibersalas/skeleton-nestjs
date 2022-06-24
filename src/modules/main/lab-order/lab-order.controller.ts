ffd88af475871a4f65df611ba2a23765392ed07a
Greiber Salas <greibersalas@gmail.com>
2022-06-24 16:43:31 -0500

    Reporte resumen orden lab xlsx

--- src/modules/main/lab-order/lab-order.controller.ts
+++ src/modules/main/lab-order/lab-order.controller.ts
@@ -177,11 +177,169 @@ export class LabOrderController {
         return response;
     }
 
+    /* --- REPORTES --- */
     @Post('/get-resume-pdf')
     async getResumePdf(@Body() filters: any): Promise<any>{
         const pdf = new Pdf_lab_resumen();
         const data = await this._labOrderService.getAllFilter(filters);
         return pdf.print(data);
+    }
+
+    @Post('/get-resume-xlsx')
+    async getReportResumeXlsx(
+        @Res() response,
+        @Body() filters: any
+    ): Promise<any>{
+        const data = await this._labOrderService.getAllFilter(filters);
+        const wb = new xl.Workbook();
+        const ws = wb.addWorksheet(`Listado`);
+        const styleTitle = wb.createStyle({
+            alignment: {
+                horizontal: ['center'],
+                vertical: ['center']
+            },
+            font: {
+                size: 14,
+                bold: true
+            }
+        });
+        ws.cell(1,1,1,11,true)
+        .string(`Resumen ordenes de laboratorio`)
+        .style(styleTitle);
+
+        let filterDate = '';
+        if (filters.option === 'e') {
+            filterDate = 'Fecha elaboración';
+        } else if (filters.option === 'i') {
+            filterDate = 'Fecha instalación';
+        } else if (filters.option === 'r') {
+            filterDate = 'Fecha registro';
+        }
+        let state = filters.state === '0' ? 'Todos' : filters.state;
+        ws.cell(2,1,2,6,true)
+        .string(``);
+        ws.cell(3,1,3,6,true)
+        .string(`Filtros: ${filterDate} Desde ${filters.since} | Hasta ${filters.until} | Estado: ${state}`);
+        ws.cell(4,1,4,6,true)
+        .string(``);
+
+        const style = wb.createStyle({
+            alignment: {
+                horizontal: ['center'],
+                vertical: ['center']
+            },
+            fill: {
+                type: 'pattern',
+                patternType: 'solid',
+                bgColor: '#000000',
+                fgColor: '#808080',
+            },
+            font: {
+                color: '#ffffff',
+                bold: true
+            }
+        });
+        ws.cell(5,1)
+        .string("Doctor")
+        .style(style);
+        ws.cell(5,2)
+        .string("Asistente dental")
+        .style(style);
+        ws.cell(5,3)
+        .string("Paciente")
+        .style(style);
+        ws.cell(5,4)
+        .string("Chip")
+        .style(style);
+        ws.cell(5,5)
+        .string("Tipo aparato")
+        .style(style);
+        ws.cell(5,6)
+        .string("Condición")
+        .style(style);
+        ws.cell(5,7)
+        .string("Color")
+        .style(style);
+        ws.cell(5,8)
+        .string("Fecha Intalación")
+        .style(style);
+        ws.cell(5,9)
+        .string("Técnico")
+        .style(style);
+        ws.cell(5,10)
+        .string("Observaciones")
+        .style(style);
+        ws.cell(5,11)
+        .string("Fecha Elaboración")
+        .style(style);
+        // size columns
+        ws.column(1).setWidth(25);
+        ws.column(2).setWidth(25);
+        ws.column(3).setWidth(40);
+        ws.column(4).setWidth(10);
+        ws.column(5).setWidth(35);
+        ws.column(6).setWidth(20);
+        ws.column(7).setWidth(23);
+        ws.column(8).setWidth(20);
+        ws.column(9).setWidth(23);
+        ws.column(10).setWidth(35);
+        ws.column(11).setWidth(23);
+        let y = 6;
+        data.map((it: any) => {
+            const {
+                doctor,
+                quotation_detail,
+                chip,
+                job,
+                color,
+                instalation,
+                elaboration,
+                technique,
+                tariff,
+                assistant,
+                observation
+            } = it;
+            const { nameQuote } = doctor;
+            const { quotation } = quotation_detail;
+            const { clinicHistory } = quotation;
+            const {
+                name,
+                lastNameFather,
+                lastNameMother
+            } = clinicHistory;
+            ws.cell(y,1)
+            .string(`${nameQuote}`);
+            ws.cell(y,2)
+            .string(`${assistant}`);
+            ws.cell(y,3)
+            .string(`${name} ${lastNameFather} ${lastNameMother}`);
+            ws.cell(y,4)
+            .string(`${chip ? 'Si' : 'No'}`);
+            ws.cell(y,5)
+            .string(`${tariff.name}`);
+            ws.cell(y,6)
+            .string(`${job}`);
+            ws.cell(y,7)
+            .string(`${color}`);
+            ws.cell(y,8)
+            .string(`${instalation}`);
+            ws.cell(y,9)
+            .string(`${technique}`);
+            ws.cell(y,10)
+            .string(`${observation}`);
+            ws.cell(y,11)
+            .string(`${elaboration}`);
+            y++;
+        });
+        await wb.writeToBuffer().then(function (buffer: any) {
+            response.set({
+                'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+                'Content-Disposition': 'attachment; filename=resume-ordenes-lab.xlsx',
+                'Content-Length': buffer.length
+            })
+
+            response.end(buffer);
+        });
     }
 
     @Get('get-cant-order/:month/:year')
@@ -192,7 +350,6 @@ export class LabOrderController {
         return this._labOrderService.getCantMonth(month,year);
     }
 
-    /* --- REPORTES --- */
     @Post('/get-report-pdf-elaborado-noelaborado')
     async getReportPdfElaboNoelabo(@Body() filters: any): Promise<any>{
         const pdf = new Pdf_report_elabo_noelabo();
