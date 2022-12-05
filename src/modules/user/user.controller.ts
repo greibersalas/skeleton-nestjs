4c27c982eb2f0453d3497b02a7fb1f9cfc61d8e9
Greiber Salas <greibersalas@gmail.com>
2022-12-05 18:44:11 -0500

    Proceso de autorización

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -11,53 +11,53 @@ import { JwtAuthGuard } from '../auth/strategies/jwt-auth.guard';
 @UseGuards(JwtAuthGuard)
 @Controller('users')
 export class UserController {
-    constructor(private readonly _userService: UserService){}
+    constructor(private readonly _userService: UserService) { }
 
     @UseGuards(JwtAuthGuard)
     @Get(':id')
-    async getUser(@Param('id',ParseIntPipe) id: number): Promise<User>{
+    async getUser(@Param('id', ParseIntPipe) id: number): Promise<User> {
         const user = await this._userService.get(id);
         return user;
     }
 
     @UseGuards(JwtAuthGuard)
     @Get()
-    async getUsers(): Promise<User[]>{
+    async getUsers(): Promise<User[]> {
         const users = await this._userService.getAll();
         return users;
     }
 
     @Post()
-    async createUser(@Body() user: User): Promise<User>{
+    async createUser(@Body() user: User): Promise<User> {
         const createUser = await this._userService.create(user);
         return createUser;
     }
 
     @Put(':id')
-    async updateUser(@Param('id',ParseIntPipe) id: number,@Body() user: User){
-        const updateUser = await this._userService.update(id,user);
+    async updateUser(@Param('id', ParseIntPipe) id: number, @Body() user: User) {
+        const updateUser = await this._userService.update(id, user);
         return updateUser;
     }
 
     @Delete(':id')
-    async deleteUser(@Param('id',ParseIntPipe) id: number){
+    async deleteUser(@Param('id', ParseIntPipe) id: number) {
         await this._userService.delete(id);
         return true;
     }
 
     @Delete('change-state/:id/:state')
     async changeStateUser(
-        @Param('id',ParseIntPipe) id: number,
-        @Param('state',ParseIntPipe) state: number,
+        @Param('id', ParseIntPipe) id: number,
+        @Param('state', ParseIntPipe) state: number,
         @Request() req: any
-    ){
-        const updateUser = await this._userService.changeState(id,state);
+    ) {
+        const updateUser = await this._userService.changeState(id, state);
         //Creamos los datos de la auditoria
         const audit = new Audit();
         audit.idregister = id;
         audit.title = 'user';
         audit.description = 'Change State';
-        audit.data = JSON.stringify({state});
+        audit.data = JSON.stringify({ state });
         audit.iduser = Number(req.user.id);
         audit.datetime = moment().format('YYYY-MM-DD HH:mm:ss');
         audit.state = 1;
@@ -65,5 +65,25 @@ export class UserController {
         await audit.save();
         //Respondemos al usuario
         return updateUser;
+    }
+
+    @Post('/autorization')
+    async AutorizationAction(
+        @Body() body: any
+    ): Promise<User> {
+        const { password, data, module } = body;
+        const user = await this._userService.autorizationUser(password);
+        //Creamos los datos de la auditoria
+        const audit = new Audit();
+        audit.idregister = data.id;
+        audit.title = module;
+        audit.description = 'Autorización';
+        audit.data = JSON.stringify({ data });
+        audit.iduser = Number(user.id);
+        audit.datetime = moment().format('YYYY-MM-DD HH:mm:ss');
+        audit.state = 1;
+        //Guardamos la auditoria
+        await audit.save();
+        return user;
     }
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,75 +1,97 @@
-import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { compare } from 'bcryptjs';
 
 import { User } from './user.entity';
 import { UserRepository } from './user.repository';
 
 @Injectable()
 export class UserService {
-    constructor(@InjectRepository(UserRepository) private readonly _userRepository:UserRepository){
+    constructor(@InjectRepository(UserRepository) private readonly _userRepository: UserRepository) {
 
     }
 
-    async get(id: number): Promise<User>{
-        if(!id){
+    async get(id: number): Promise<User> {
+        if (!id) {
             throw new BadRequestException('id must be send');
         }
         //const user = await this._userRepository.findOne(id,{where:{estado:1}});
         const user = await this._userRepository.createQueryBuilder("us")
-        .innerJoinAndSelect("us.roles","rl")
-        .leftJoinAndSelect("us.doctor","dc")
-        .where("us.id = :id AND us.estado <> 0",{id}).getOne();
-        if(!user){
+            .innerJoinAndSelect("us.roles", "rl")
+            .leftJoinAndSelect("us.doctor", "dc")
+            .where("us.id = :id AND us.estado <> 0", { id }).getOne();
+        if (!user) {
             throw new NotFoundException();
         }
         user.password = '****';
         return user;
     }
 
-    async getAll(): Promise<User[]>{
+    async getAll(): Promise<User[]> {
         const users: User[] = await this._userRepository.createQueryBuilder('us')
-        .innerJoinAndSelect('us.roles','ro')
-        .where('us.estado <> 0')
-        .getMany();
+            .innerJoinAndSelect('us.roles', 'ro')
+            .where('us.estado <> 0')
+            .getMany();
 
         return users;
     }
 
-    async create(user: User): Promise<User>{
+    async create(user: User): Promise<User> {
         const saveUser: User = await this._userRepository.save(user);
         return saveUser;
     }
 
-    async update(id: number, user: User): Promise<any>{
+    async update(id: number, user: User): Promise<any> {
         //const update = await this._userRepository.update(id,user);
         const update = await this._userRepository.createQueryBuilder()
-        .update(User).set({
-            username: user.username,
-            email: user.email,
-            roles: user.roles,
-            doctor: user.doctor,
-            campus: user.campus
-        }).where({id}).execute();
+            .update(User).set({
+                username: user.username,
+                email: user.email,
+                roles: user.roles,
+                doctor: user.doctor,
+                campus: user.campus
+            }).where({ id }).execute();
         return update;
     }
 
-    async delete(id: number): Promise<void>{
-        const userExists = await this._userRepository.findOne(id,{where:{estado:1}});
+    async delete(id: number): Promise<void> {
+        const userExists = await this._userRepository.findOne(id, { where: { estado: 1 } });
 
-        if(!userExists){
+        if (!userExists) {
             throw new NotFoundException();
         }
 
-        await this._userRepository.update(id,{estado:0});
+        await this._userRepository.update(id, { estado: 0 });
     }
 
-    async changeState(id: number,state: number): Promise<any>{
+    async changeState(id: number, state: number): Promise<any> {
         const userExists = await this._userRepository.findOne(id);
 
-        if(!userExists){
+        if (!userExists) {
             throw new NotFoundException();
         }
 
-        return await this._userRepository.update(id,{estado: state});
+        return await this._userRepository.update(id, { estado: state });
+    }
+
+    async autorizationUser(password: string): Promise<User> {
+        const users: User[] = await this._userRepository.createQueryBuilder("us")
+            .innerJoinAndSelect("us.roles", "rl")
+            .where('us.estado = 1')
+            .andWhere('rl.idrole IN(1,2)')
+            .getMany();
+        if (!users) {
+            throw new NotFoundException('User does not exist');
+        }
+        let isMatch = false;
+        for await (const user of users) {
+            isMatch = await compare(password, user.password);
+            if (isMatch) {
+                return user
+            }
+        }
+        if (!isMatch) {
+            throw new UnauthorizedException('invalid credentials');
+        }
     }
 }
